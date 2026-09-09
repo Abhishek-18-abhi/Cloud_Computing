edbd20aae393f3f6ebfcab61ab66047a8306f6cb
Omkar Parkar <omkarparkar885@gmail.com>
2026-09-09 20:28:49 +0530

    Update project files

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { CartProvider } from "./context/CartContext";
 import { AuthProvider } from "./context/AuthContext";
 
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
